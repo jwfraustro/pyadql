679debf06b0d20293eaa2a885013177470def367
Joshua Fraustro <jfraustro@stsci.edu>
2024-04-09 11:03:54 -0400

    fix parsing comparison operators

--- pyadql/parser.py
+++
@@ -1,7 +0,0 @@
-from lark import Lark, tree
-
-adql_parser = Lark.open("adql.lark", rel_to=__file__, start="query_expression")
-
-text = "SELECT TOP 10 col_1, col_2 AS col_a, col_b FROM dbo.tap_schema.my_table"
-parsed = adql_parser.parse(text)
-tree.pydot__tree_to_png(parsed, "parsed.png")
