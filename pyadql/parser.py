7c7e3736cd5af355439b17afb20dc09b4c45fa25
jwfraustro <36318163+jwfraustro@users.noreply.github.com>
2024-12-08 18:19:14 -0500

    add adql21

--- pyadql/parser.py
+++ pyadql/parser.py
@@ -1,14 +1,15 @@
 from lark import Lark, tree
 
-from pyadql.classes.translator import ADQLTranslator
 
 adql_parser = Lark.open(
-    "grammars/adql-2.0/adql-2.0.lark", rel_to=__file__, start="query_expression", maybe_placeholders=False
+    "grammars/adql-2.0/adql-2.0.lark",
+    rel_to=__file__,
+    start="query_expression",
+    maybe_placeholders=False,
 )
 
 
 text = "SELECT TOP 10 * FROM dbo.tap_schema.my_table"
 
 parsed = adql_parser.parse(text)
-out_tree = ADQLTranslator().transform(parsed)
-tree.pydot__tree_to_png(out_tree, "parsed.png")
+tree.pydot__tree_to_png(parsed, "parsed.png")
