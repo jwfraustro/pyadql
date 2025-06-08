c8aa61676f1a431e5f9ab24ec332d7fcae2576f1
jwfraustro <36318163+jwfraustro@users.noreply.github.com>
2025-06-08 14:51:23 -0400

    switch to ADQL 2.1

--- pyadql/parser.py
+++ pyadql/parser.py
@@ -1,15 +1,14 @@
 from lark import Lark, tree
 
-
 adql_parser = Lark.open(
-    "grammars/adql-2.0/adql-2.0.lark",
+    "grammars/adql-2.1/adql2.1.lark",
     rel_to=__file__,
     start="query_expression",
     maybe_placeholders=False,
 )
 
 
-text = "SELECT TOP 10 * FROM dbo.tap_schema.my_table"
+text = "select x, my_column FROM y WHERE z=0 AND v>2"
 
 parsed = adql_parser.parse(text)
 tree.pydot__tree_to_png(parsed, "parsed.png")
